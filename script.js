81f3f84ddc80b17fac4afc0eae518169257ea879
Stubberson <tuukka.j.korpela@gmail.com>
2026-03-26 09:44:39 +0100

    Refactor book into a class

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-let bookList = document.querySelector('ul')
-
+const bookList = document.querySelector('ul')
 const showDialogButton = document.querySelector('#new-book')
-
 const newBookDialog = document.querySelector('dialog')
 const dialogCloseButton = document.querySelector('dialog button')
 const dialogForm = document.querySelector('form')
@@ -11,64 +9,53 @@ const bookAuthor = document.querySelector('#author')
 const bookPage = document.querySelector('#page')
 const bookFinished = document.querySelector('#finished')
 
-let myLibrary = []
+class Book {
+  static library = []
 
-function Book(title, author, pages, finished) {
-  if (!new.target) {
-    throw Error("Use the 'new' operator to call the constructor!")
+  constructor(title, author, pages, finished) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.finished = finished
+    this.id = crypto.randomUUID()
+
+    Book.library.push(this)
   }
 
-  this.title = title
-  this.author = author
-  this.pages = pages
-  this.finished = finished
-  this.id = crypto.randomUUID()
+  info() {
+    return `${this.title} by ${this.author}, pages ${this.pages}, ${this.finished ? 'finished' : 'unfinished'}`
+  }
 
-  this.info = function() {
-    if (this.finished) {
-      return `${this.title} by ${this.author}, finished`
-    } else {
-      return `${this.title} by ${this.author}, at page ${this.pages}, unfinished`
-    }
+  displayBook() {
+    const listItem = document.createElement('li')
+    listItem.innerText = this.info()
+
+    const finishedCheckbox = document.createElement('input')
+    finishedCheckbox.type = 'checkbox'
+    finishedCheckbox.className = 'checkbox'
+    this.finished ? finishedCheckbox.checked = true : finishedCheckbox.checked = false
+
+    const removeButton = document.createElement('button')
+    removeButton.innerText = 'Remove'
+
+    bookList.appendChild(listItem)
+    listItem.appendChild(finishedCheckbox)
+    listItem.appendChild(removeButton)
+
+    finishedCheckbox.addEventListener('click', () => {
+      finishedCheckbox.checked ? this.finished = true : this.finished = false
+      let updatedText = document.createTextNode(this.info())
+      listItem.replaceChild(updatedText, listItem.firstChild)  // Update only the text from the listItem
+    })
+
+    removeButton.addEventListener('click', () => {
+      listItem.remove()  // Remove from DOM
+      Book.library.splice(Book.library.indexOf(this), 1)  // Remove from memory
+    })
   }
 }
 
-function addBookToLibrary(title, author, pages, finished) {
-  book = new Book(title, author, pages, finished)
-  myLibrary.push(book)
-  return book
-}
-
-// DISPLAY LIBRARY ITEM W/ BUTTONS
-function displayNewBook(book) {
-  const listItem = document.createElement('li')
-  listItem.innerText = book.info()
-
-  const finishedCheckbox = document.createElement('input')
-  finishedCheckbox.type = 'checkbox'
-  finishedCheckbox.className = 'checkbox'
-  book.finished ? finishedCheckbox.checked = true : finishedCheckbox.checked = false
-
-  const removeButton = document.createElement('button')
-  removeButton.innerText = 'Remove'
-
-  bookList.appendChild(listItem)
-  listItem.appendChild(finishedCheckbox)
-  listItem.appendChild(removeButton)
-
-  finishedCheckbox.addEventListener('click', () => {
-    finishedCheckbox.checked ? book.finished = true : book.finished = false
-    let updatedText = document.createTextNode(book.info())
-    listItem.replaceChild(updatedText, listItem.firstChild)  // Update only the text from the listItem
-  })
-
-  removeButton.addEventListener('click', () => {
-    listItem.remove()  // Remove from DOM
-    myLibrary.splice(myLibrary.indexOf(book), 1)  // Remove from memory
-  })
-}
-
-// ADD BOOK DIALOG
+// BOOK DIALOG
 showDialogButton.addEventListener('click', () => {
   newBookDialog.showModal()
 })
@@ -78,9 +65,9 @@ dialogCloseButton.addEventListener('click', () => {
 })
 
 dialogSubmitButton.addEventListener('click', () => {
-  if (!myLibrary.some(book => book.title === bookTitle.value)) {  // Don't allow to add the same book multiple times
-    let newBook = addBookToLibrary(bookTitle.value, bookAuthor.value, bookPage.value, bookFinished.checked)
-    displayNewBook(newBook)
+  if (!Book.library.some(book => book.title === bookTitle.value)) {
+    let newBook = new Book(bookTitle.value, bookAuthor.value, bookPage.value, bookFinished.checked)
+    newBook.displayBook()
   }
   
   dialogForm.reset()  // The <dialog> element doesn't reset the form automatically when submitted
